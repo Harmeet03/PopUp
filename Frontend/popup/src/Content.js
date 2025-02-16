7de338a2b21021008ec7e2c2820fedace870dd66
Harmeet Singh <hsdhanjal2003@gmail.com>
2025-02-16 23:56:57 +0530

    Basic Layout

--- Frontend/popup/src/Content.js
+++ Frontend/popup/src/Content.js
@@ -1,7 +1,42 @@
+import React, { useEffect, useState } from "react";
+
 const Content = () => {
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        fetchProduct()
+    }, [])
+
+    const fetchProduct = async () => {
+        try{
+            const response =  await fetch("/Products.json")
+            const data = await response.json()  ;
+            setProduct(data[0]);
+            if(response.ok){
+                console.log('Fetch Success');
+            }
+            else{
+                console.log('Fetch Failed')
+            }
+        }
+        catch(error){
+            console.log('Error ', error)
+        }
+    }
+
     return(
         <>
-            
+            {
+                product ? (
+                    <div>
+                        <img src={product.shirt[0].image} width={250}/>
+                        <p> Product: {product.shirt[0].name} </p>
+                        <p> Price: {product.shirt[0].price} </p>
+                    </div>
+                ) : (
+                    <p> Loading </p>
+                )
+            }
         </>
     )
 }
--- Frontend/popup/src/Nav.js
+++ Frontend/popup/src/Nav.js
@@ -1,6 +1,9 @@
+import { use } from 'react';
 import Logo from './Media/Logo.png'
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
+    const To = useNavigate();
     return(
         <>
             <nav style={{background: 'black', color: 'white', display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
@@ -8,9 +11,9 @@ const Nav = () => {
                     <img src={Logo} width={'150px'}/>
                 </div>
                 <div className="links" style={{display: 'flex', gap: '20px'}}>
-                    <p> Shirt </p>
-                    <p> Pant </p>
-                    <p> Shoes </p>
+                    <p onClick={() => {To('/')}} style={{cursor: 'pointer'}}> Shirt </p>
+                    <p onClick={() => {To('/')}} style={{cursor: 'pointer'}}> Pant </p>
+                    <p onClick={() => {To('/')}} style={{cursor: 'pointer'}}> Shoes </p>
                 </div>
             </nav>
         </>
